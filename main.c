403263f1ffd3134cfb31e7aea0a34a8bb53a427f
JulioCamos <117041567+JulioCamos@users.noreply.github.com>
2022-11-02 12:19:51 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ void main(){
     const int tamxarray1 = 3;
     const int tamyarray1 = 3;
 
-    int array[3][3] = {
+    int array1[3][3] = {
         {9, 1, 5},
         {2, 3, 0},
         {1, 5, 7}
@@ -21,4 +21,17 @@ void main(){
         {0, 0, 4},
         {4, 7, 0}
     };
+
+    Matrixinfo* Matriz1 = loadlist(tamxarray1, tamyarray1, array1);
+    printmatrix(Matriz1);
+
+    printf("\n");
+
+    Matrixinfo* Matriz2 = loadlist(tamxarray1, tamyarray2, array2);
+    printmatrix(Matriz2);
+
+    printf("\n");
+
+    Matriz1 = multiply(Matriz1, Matriz2);
+    printmatrix(Matriz1);
 }
